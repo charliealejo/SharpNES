33440da47ee1f9d4b267f6b9f2d6695c51d881ad
Carlos.Alejo <Carlos.Alejo@bestsecret.com>
2025-08-17 23:35:54 +0200

    Added bitwise instructions

---
+++ Ricoh6502/Commands/BIT.cs
@@ -0,0 +1,15 @@
+namespace Ricoh6502.Commands
+{
+    public abstract class BIT : BoundaryCheckCommandBase
+    {
+        public BIT(AddressingMode addressingMode, byte d1, byte d2) : base(addressingMode, d1, d2) { }
+
+        protected override void ExecuteInternal(Processor processor)
+        {
+            byte value = processor.GetValue(AddressingMode, D1, D2);
+            byte result = (byte)(processor.Acc & value);
+            processor.Status.SetZeroAndNegativeFlags(result);
+            processor.Status.OverflowFlag = (value & 0x40) != 0;
+        }
+    }
+}
---
+++ Ricoh6502/Commands/ORA.cs
@@ -0,0 +1,14 @@
+namespace Ricoh6502.Commands
+{
+    public abstract class ORA : BoundaryCheckCommandBase
+    {
+        public ORA(AddressingMode addressingMode, byte d1, byte d2) : base(addressingMode, d1, d2) { }
+
+        protected override void ExecuteInternal(Processor processor)
+        {
+            byte value = processor.GetValue(AddressingMode, D1, D2);
+            processor.Acc = (byte)(processor.Acc | value);
+            processor.Status.SetZeroAndNegativeFlags(processor.Acc);
+        }
+    }
+}
